2e086153582325d4b11a7e6a1efc44a3deb65107
Salma Guevara <ada_hack421@hackademy.mx>
2023-03-23 11:07:16 -0600

    Actions menu was added to the Login class, so that once the session starts, you can choose operations to perform.

--- src/main/kotlin/user/Login.kt
+++ src/main/kotlin/user/Login.kt
@@ -12,6 +12,38 @@ class Login {
             val currentPassword = readln().toString()
             if(currentPassword == password){
                 println("\nBienvenid@ ${name}, tu sesión se ha iniciada correctamente.")
+                println("\nElige la siguiente acción " +
+                        "\n1. Ver productos" +
+                        "\n2. Pagos" +
+                        "\n3. Envíos" +
+                        "\n4. Devoluciones" +
+                        "\n5. Preguntas frecuentes")
+
+                when (readln().toInt()) {
+                    1 -> {
+                        //Agregar funcionalidad
+                       println("Lista de productos")
+                    }
+                    2 -> {
+                        //Agregar funcionalidad
+                        println("Estás pagando")
+                    }
+                    3 -> {
+                        //Agregar funcionalidad
+                        println("Checando envíos")
+                    }
+                    4 -> {
+                        //Agregar funcionalidad
+                        println("Checando devoluciones")
+                    }
+                    5 -> {
+                        //Agregar funcionalidad
+                        println("Preguntas frecuentes")
+                    }
+                    else -> {
+                        println("Ups, algo salió mal, por favor ingresa una opción válida")
+                    }
+                }
             }else{
                 println("\nLo sentimos, los datos introducidos no son correctos :(")
                 login(email, password, name)
